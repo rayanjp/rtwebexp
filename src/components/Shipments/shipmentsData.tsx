baba5cd94886695c1231ebf83786f66fa446ba47
rayanjp <rayan12oct@gmail.com>
2025-09-22 07:19:50 +0000

    changed

--- src/components/Shipments/shipmentsData.tsx
+++ src/components/Shipments/shipmentsData.tsx
@@ -2,20 +2,7 @@
 import { Shipment } from "@/types/shipment";
 
 const shipmentsData: Shipment[] = [
-  {
-    "id": "1",
-    "title": "Toyota Corolla",
-    "model": "Sedan",
-    "year": 2019,
-    "destination": "Australia",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-aus/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-aus/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-aus/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-aus/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-aus/4.png"
-    ]
-  },
+  
   {
     "id": "2",
     "title": "Audi A3",
@@ -31,51 +18,7 @@ const shipmentsData: Shipment[] = [
       "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/audi-a3-uk1/5.jpg"
     ]
   },
-  {
-    "id": "3",
-    "title": "Nissan Diesel Tanker",
-    "model": "",
-    "year": 2001,
-    "destination": "Tanzania",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-truck-tanzania/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-truck-tanzania/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-truck-tanzania/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-truck-tanzania/3.png"
-    ]
-  },
-  {
-    "id": "4",
-    "title": "Toyota Estima Hybrid",
-    "model": "Aeras Premium",
-    "year": 2018,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk1/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk1/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk1/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk1/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk1/4.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk1/5.png"
-    ]
-  },
-  {
-    "id": "5",
-    "title": "Toyota Raize",
-    "model": "G grade",
-    "year": 2020,
-    "destination": "Pakistan",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/4.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/5.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/6.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-raize-pk1/7.png"
-    ]
-  },
+  
   {
     "id": "6",
     "title": "Lexus LX 570",
@@ -93,110 +36,7 @@ const shipmentsData: Shipment[] = [
       "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/lexus-lx570-uk1/7.jpg"
     ]
   },
-  {
-    "id": "7",
-    "title": "Toyota Tacoma",
-    "model": "4WD",
-    "year": 2006,
-    "destination": "UAE",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-tacoma-uae/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-tacoma-uae/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-tacoma-uae/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-tacoma-uae/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-tacoma-uae/4.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-tacoma-uae/5.png"
-    ]
-  },
-  {
-    "id": "8",
-    "title": "Toyota Prius Alpha",
-    "model": "S Tune Black II",
-    "year": 2018,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/prius-alpha-uk1/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/prius-alpha-uk1/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/prius-alpha-uk1/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/prius-alpha-uk1/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/prius-alpha-uk1/4.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/prius-alpha-uk1/5.png"
-    ]
-  },
-  {
-    "id": "9",
-    "title": "Toyota Estima Hybrid",
-    "model": "Aeras Smart",
-    "year": 2018,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk2/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk2/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk2/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk2/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-estima-uk2/4.png"
-    ]
-  },
-  {
-    "id": "10",
-    "title": "Toyota Voxy Hybrid",
-    "model": "X",
-    "year": 2020,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-voxy-uk1/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-voxy-uk1/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-voxy-uk1/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-voxy-uk1/3.png"
-    ]
-  },
-  {
-    "id": "11",
-    "title": "Toyota Corolla Touring Hybrid",
-    "model": "",
-    "year": 2020,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/4.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/5.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-corolla-touring-uk1/6.png"
-    ]
-  },
-  {
-    "id": "12",
-    "title": "Honda Fit Hybrid",
-    "model": "GP5",
-    "year": 2014,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/honda-fit-uk1/1.png",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/honda-fit-uk1/1.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/honda-fit-uk1/2.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/honda-fit-uk1/3.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/honda-fit-uk1/4.png",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/honda-fit-uk1/5.png"
-    ]
-  },
-  {
-    "id": "13",
-    "title": "Nissan Note e-Power",
-    "model": "X-V",
-    "year": 2019,
-    "destination": "Pakistan",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/1.jpg",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/1.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/2.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/3.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/4.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/5.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/nissan-note-pk1/6.jpg"
-    ]
-  },
+ 
   {
     "id": "14",
     "title": "Toyota CH-R Hybrid",
@@ -213,55 +53,7 @@ const shipmentsData: Shipment[] = [
       "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-pk1/6.jpg"
     ]
   },
-  {
-    "id": "15",
-    "title": "Toyota Prius",
-    "model": "S Grade",
-    "year": 2019,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-prius-uk1/1.jpg",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-prius-uk1/1.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-prius-uk1/2.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-prius-uk1/3.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-prius-uk1/4.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-prius-uk1/5.jpg"
-    ]
-  },
-  {
-    "id": "16",
-    "title": "Toyota CH-R",
-    "model": "",
-    "year": 2019,
-    "destination": "Tanzania",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/1.jpg",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/1.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/2.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/3.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/4.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/5.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/6.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-chr-tanzania/7.jpg"
-    ]
-  },
-  {
-    "id": "17",
-    "title": "Toyota Noah Hybrid",
-    "model": "",
-    "year": 2019,
-    "destination": "UK",
-    "cover": "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-noah-uk1/1.jpg",
-    "images": [
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-noah-uk1/1.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-noah-uk1/2.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-noah-uk1/3.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-noah-uk1/4.jpg",
-      "https://vtdzsj3rex0zyah3.public.blob.vercel-storage.com/shipments/toyota-noah-uk1/5.jpg"
-    ]
-  },
-
-
+  
 ];
 
 export default shipmentsData;
